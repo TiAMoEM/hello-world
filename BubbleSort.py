7e2d8b190617a888b88c3f9e03c775887b895815
yangjiayi <415878598@qq.com>
2018-06-29 09:54:40 +0800

    冒泡排序算法

---
+++ BubbleSort.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+
+def BubbleSort(arr):
+    for i in range(len(arr) - 1):
+        for j in range(i, len(arr) - 1):
+            if arr[j + 1] < arr[j]:
+                arr[j + 1], arr[j] = arr[j], arr[j + 1]
+    return arr
+
+
+if __name__ == '__main__':
+    arr = [1, 2, 3, 4, 6, 5, 4]
+    print(BubbleSort(arr))
+
