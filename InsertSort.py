e81757894fdbb5c0f8f715be7f36385c4c663846
yangjiayi <415878598@qq.com>
2018-06-30 11:37:35 +0800

    插入排序

---
+++ InsertSort.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+
+def InsertSort(arr):
+    for i in range(1, len(arr))
+        key = arr[i]
+        j = i - 1
+        while j >= 0:
+            if arr[j] > key:
+                arr[j + 1], arr[j] = arr[j], key
+            j -= 1
+    return arr
+
+
