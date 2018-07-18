36270519d7b035df701a5bacb149aa2e196010f1
yangjiayi <415878598@qq.com>
2018-07-18 14:54:26 +0800

    调整冒泡排序

--- algorithm/BubbleSort.py
+++ algorithm/BubbleSort.py
@@ -2,7 +2,7 @@
 
 def BubbleSort(arr):
     for i in range(len(arr) - 1):
-        for j in range(i, len(arr) - 1):
+        for j in range(len(arr)- i - 1):
             if arr[j + 1] < arr[j]:
                 arr[j + 1], arr[j] = arr[j], arr[j + 1]
     return arr
